dd4cfa950ef9cde2e340843a59f2c28e3b2ac471
Edinei J. Bauer <galera.org@gmail.com>
2019-07-23 09:52:55 -0300

    ignora entidades do sistema na edição das entidades

--- public/set/load/dicionarios.php
+++ public/set/load/dicionarios.php
@@ -1,7 +1,7 @@
 <?php
 $data['data'] = [];
 foreach (\Helpers\Helper::listFolder("entity/cache") as $json) {
-    if($json !== "info" && preg_match('/\.json$/i', $json)) {
+    if($json !== "info" && !in_array($json, ["api_chave.json", "config.json", "dashboard_note.json", "dashboard_push.json", "email_envio.json", "login_attempt.json", "usuarios.json"]) && preg_match('/\.json$/i', $json)) {
         $name = str_replace('.json', '', $json);
         $dados = \Entity\Metadados::getDicionario($name, null, true);
         if($dados && count($dados) > 0) {
