420961196440238e981550d1711ea340b2bb77f9
Edinei J. Bauer <galera.org@gmail.com>
2020-05-28 20:35:18 -0300

    static function input default

--- public/src/EntityUi/InputType.php
+++ public/src/EntityUi/InputType.php
@@ -29,4 +29,9 @@ class InputType
 
         return $result;
     }
+
+    public static function getInputDefault()
+    {
+        return json_decode(file_get_contents(PATH_HOME . VENDOR . "entity-ui/public/input_type/default.json"), !0)['default'];
+    }
 }
