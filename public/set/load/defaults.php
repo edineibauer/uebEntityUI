5d4badfcf912eef47830468c7e831a70edb8618c
Edinei J. Bauer <galera.org@gmail.com>
2020-05-28 21:46:43 -0300

    fix defaults load

--- public/set/load/defaults.php
+++ public/set/load/defaults.php
@@ -1,2 +1,2 @@
 <?php
-$data['data'] = json_decode(file_get_contents(PATH_HOME . VENDOR . "entity-ui/public/input_type/default.json"), !0);
+$data['data'] = array_merge(['default' => \EntityUi\InputType::getInputDefault()], \EntityUi\InputType::getInputTypes());
