0e6bf2ee24cfa2b86e0482e144fc0edfd3285cf9
Edinei J. Bauer <galera.org@gmail.com>
2020-05-29 08:29:58 -0300

    input setor type

--- public/assets/inputSetor.js
+++ public/assets/inputSetor.js
@@ -1,5 +1,9 @@
 get("setores").then(setores => {
-    $("#setor").html('<option value="" selected="selected" disabled="disabled">selecione</option>');
-    for(let setor of setores)
-        $("#setor").append('<option value="' + setor + '">' + setor  + '</option>');
+    $(".inputSetor").each(function(i, e) {
+        let $setor = $(e).find("select");
+        $setor.html('<option value="" selected="selected" disabled="disabled">selecione</option>');
+        let id = $setor.attr("id");
+        for(let setor of setores)
+            $setor.append('<option value="' + setor + '"' + (!isEmpty(form.data[id]) && setor === form.data[id] ? ' selected="selected"' : '') + '>' + ucFirst(setor.replace("_", " ").replace("_", " "))  + '</option>');
+    });
 });
