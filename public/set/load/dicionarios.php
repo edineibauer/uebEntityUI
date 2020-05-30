2f9848c1bc6c4e5e9c3f50a5464eaf4146cde0e7
Edinei J. Bauer <galera.org@gmail.com>
2020-05-30 11:38:52 -0300

    update dicionarios to not show on entity UI

--- public/set/load/dicionarios.php
+++ public/set/load/dicionarios.php
@@ -1,15 +1,33 @@
 <?php
 $data['data'] = [];
+
+$listNot = [
+    "api_chave.json",
+    "enviar_mensagem.json",
+    "notifications.json",
+    "notifications_report.json",
+    "push_notifications.json",
+    "email_envio.json",
+    "login_attempt.json",
+    "usuarios_token.json",
+    "relatorios.json",
+    "relatorios_card.json",
+    "relatorios_regras.json",
+    "relatorios_filtro.json",
+    "relatorios_grupos.json",
+    "cep.json"
+];
+
 foreach (\Helpers\Helper::listFolder("entity/cache") as $json) {
-    if($json !== "info" && !in_array($json, ["api_chave.json", "config.json", "dashboard_note.json", "dashboard_push.json", "email_envio.json", "login_attempt.json", "usuarios_token.json"]) && preg_match('/\.json$/i', $json)) {
+    if ($json !== "info" && !in_array($json, $listNot) && preg_match('/\.json$/i', $json)) {
         $name = str_replace('.json', '', $json);
         $dados = \Entity\Metadados::getDicionario($name, null, true);
-        if($dados && count($dados) > 0) {
+        if ($dados && count($dados) > 0) {
             $e = 1;
             foreach ($dados as $i => $dado) {
-                if($dado['column'] === "autorpub" || $dado['column'] === "ownerpub") {
+                if ($dado['column'] === "autorpub" || $dado['column'] === "ownerpub") {
                     unset($dados[$i]);
-                } elseif(empty($dado['indice'])) {
+                } elseif (empty($dado['indice'])) {
                     $dados[$i]['indice'] = $e;
                     $e++;
                 }
