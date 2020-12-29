76a5d9c14dd301ad705319edd2c81090fa95897a
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-29 13:59:31 -0300

    wcache db update ownerpub

--- public/src/EntityUi/EntityCreateEntityDatabase.php
+++ public/src/EntityUi/EntityCreateEntityDatabase.php
@@ -29,10 +29,7 @@ class EntityCreateEntityDatabase extends EntityDatabase
         /**
          * Cache DB
          */
-        $sql->exeCommand(
-            "DROP TABLE IF EXISTS " . PRE . "wcache_" . $entity . ";"
-            . "CREATE TABLE IF NOT EXISTS `" . PRE . "wcache_" . $entity . "` (`id` INT(11) NOT NULL, `system_id` INT(11) DEFAULT NULL, `data` longtext DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
-            . "ALTER TABLE `" . PRE . "wcache_" . $entity . "` ADD PRIMARY KEY (`id`), MODIFY `id` int(11) NOT NULL AUTO_INCREMENT", !0);
+        Config::createCacheDB($entity);
     }
 
     private  function createEntityFromJson(string $lib, string $entity) {
